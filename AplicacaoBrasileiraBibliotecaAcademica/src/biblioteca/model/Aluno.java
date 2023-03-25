6de6f1b07ab0247672b1a1a9e28da31a5d87cf9a
Abner Andrade <aa.abnerandrade@gmail.com>
2023-03-24 22:12:07 -0300

    Revisão e Ajustes de Menu, SubMenus e Relações entre Classes

--- AplicacaoBrasileiraBibliotecaAcademica/src/biblioteca/model/Aluno.java
+++ AplicacaoBrasileiraBibliotecaAcademica/src/biblioteca/model/Aluno.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Aluno extends Usuario {
     private String matricula;
     private ArrayList<Emprestimo> emprestimosDoAluno = new ArrayList();
-    private ArrayList<Livro> livrosComAluno = new ArrayList();
+    private ArrayList<Livro> livrosComAluno = new ArrayList(); // ForJoyce: (4) Aqui pode não ser Array. Prevemos apenas 1 livro por empréstimo. E apenas 1 empréstimo por vez.
 
     public Aluno(String nome, String cpf, String matricula) {
         super(nome, cpf);
